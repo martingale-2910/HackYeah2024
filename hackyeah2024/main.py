045c447ab53cd335d3942fe7d014a366948191d9
Grzegorz Bokota <bokota+github@gmail.com>
2024-09-28 18:05:21 +0200

    update project structure

--- hackyeah2024/main.py
+++ hackyeah2024/main.py
@@ -5,19 +5,19 @@ from hackyeah2024.server import run_server
 
 @click.group()
 def main():
-    print("Running main CLI")
+    print('Running main CLI')
 
 
 @main.command()
 def dummy():
-    print("Running a dummy script")
+    print('Running a dummy script')
 
 
 @main.command()
 def server():
-    print("Running server")
+    print('Running server')
     run_server()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
