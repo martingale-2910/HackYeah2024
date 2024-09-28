46230f429923cd0207902c89c56d0de8b5c434d0
martingale <124511455+martingale-2910@users.noreply.github.com>
2024-09-28 22:08:23 +0200

    Add locality resolution + selectbox

--- hackyeah2024/server.py
+++ hackyeah2024/server.py
@@ -1,6 +1,6 @@
 import folium
 import streamlit as st
-from osmnx import distance, geocoder, graph_from_bbox, routing
+from osmnx import distance, geocoder, graph_from_bbox, routing, features_from_bbox
 from streamlit_folium import st_folium
 
 bounding_box = {'min_lat': 49.174522, 'max_lat': 50.526524, 'min_lon': 19.076385, 'max_lon': 21.432953}
@@ -31,26 +31,23 @@ def plot_route(map_, route_pts):
 def run_server():
     st.title('Rowerem przez Małopolskę')
 
+    localities = tuple(features_from_bbox(
+        bbox=(bounding_box['max_lat'], bounding_box['min_lat'], bounding_box['min_lon'], bounding_box['max_lon']),
+        tags={"place": ["town", "vilage"]})['name'].values
+        )
+
     cont1 = st.container()
 
     with cont1:
         col1, col2 = st.columns(2)
 
         with col1:
-            st.text_input('Skąd')
+            origin = st.selectbox("Skąd", localities)
 
         with col2:
-            st.text_input('Dokąd')
+            destination = st.selectbox("Dokąd", localities)
 
     cont2 = st.container()
-
-    map_bound_coordinates = [
-        (50.526524, 19.076385),
-        (50.526524, 21.432953),
-        (49.174522, 21.432953),
-        (49.174522, 19.076385),
-        (50.526524, 19.076385),
-    ]
 
     # center on Liberty Bell, add marker
     map_ = folium.Map(
@@ -66,9 +63,9 @@ def run_server():
     marker = folium.Marker([50.049683, 19.944544], popup='Kraków', tooltip='Kraków')
     map_.add_child(marker)
 
-    map_bounds = folium.PolyLine(locations=map_bound_coordinates, weight=5)
+    route_pts = get_route_points(origin, destination)
 
-    map_.add_child(map_bounds)
+    plot_route(map_, route_pts)
 
     with cont2:
         # call to render Folium map in Streamlit
