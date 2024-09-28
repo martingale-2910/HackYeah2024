b4115289ddab8a0707cca7c817391cf1b2260d1f
martingale <124511455+martingale-2910@users.noreply.github.com>
2024-09-28 20:28:37 +0200

    Update coords

--- hackyeah2024/server.py
+++ hackyeah2024/server.py
@@ -4,6 +4,12 @@ import streamlit as st
 from streamlit_folium import st_folium
 
 
+def plot_route(map, route_pts):
+    route=folium.PolyLine(locations=route_pts, weight=2)
+    map.add_child(route)
+    return map
+
+
 def run_server():
     st.title('Rowerem przez Małopolskę')
 
@@ -21,27 +27,23 @@ def run_server():
     cont2 = st.container()
 
     map_bound_coordinates=[
-        (50.581493, 17.803345),
-        (50.581493, 22.884521),
-        (48.915280, 22.884521),
-        (48.915280, 17.803345),
-        (50.581493, 17.803345),
+        (50.526524, 19.076385),
+        (50.526524, 21.432953),
+        (49.174522, 21.432953),
+        (49.174522, 19.076385),
+        (50.526524, 19.076385),
     ]
 
     # center on Liberty Bell, add marker
     map = folium.Map(
         max_bounds=True,
-        location=[50.049683, 19.944544],
+        location=[0.5*(49.174522 + 50.526524), 0.5*(19.944544 + 21.432953)],
         zoom_start=9,
-        min_lat=48.915280,
-        max_lat=50.581493,
-        min_lon=17.803345,
-        max_lon=22.884521,
-        tiles='OpenStreetMap'
-        # min_lon=19.076385,
-        # max_lon=21.432953,
-        # min_lat=49.174522,
-        # max_lat=50.526524
+        tiles='OpenStreetMap',
+        min_lon=19.076385,
+        max_lon=21.432953,
+        min_lat=49.174522,
+        max_lat=50.526524
     )
     marker = folium.Marker([50.049683, 19.944544], popup='Kraków', tooltip='Kraków')
     map.add_child(marker)
